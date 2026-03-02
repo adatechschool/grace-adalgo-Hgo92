3c085a3aa78f376f615e3611fb71735dc4424f4f
Hgo92 <hugo_monier@yahoo.fr>
2026-03-02 14:40:36 +0100

    Exercice 2

---
+++ src/2-FIlterLetter.ts
@@ -0,0 +1,32 @@
+/*
+This function checks whether a given argument is a letter or not.
+
+The function takes three parameters:
+
+- arg: the value to check
+- call: a callback function executed if the argument is a letter
+- call2: a callback function executed if the argument is NOT a letter
+
+If the argument is not a number, the function should execute the first callback.
+If the argument is a number, the function should execute the second callback.
+
+The function should return the result of the executed callback.
+
+For example:
+
+filterLetter("a", callback1, callback2)
+// should execute callback1
+
+filterLetter("8", callback1, callback2)
+// should execute callback2
+*/
+
+export const filterLetter = (arg : string, call : Function, call2 : Function) => {
+    if (arg.length === 1 && arg.match(/[a-z]/i)) {
+        call(arg)
+    } else if (arg.match(/[0-9]/i)) {
+        call2(arg)
+    } else {
+        call(arg)
+    }
+};
